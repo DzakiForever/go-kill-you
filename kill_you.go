7235e0f41a4928cb5e93f4a5106af9baf78431c5
Dzaki <moh.dzaki.ayatillah@gmail.com>
2023-01-27 14:02:36 +0800

    update kill you3

--- kill_you.go
+++ kill_you.go
@@ -1,5 +1,26 @@
 package go_kill_you
 
+import (
+	"fmt"
+)
+
 func KillYou(name string) string {
 	return "I really want to kill you " + name
+}
+
+func PersonId() {
+	var name string
+	var age string
+	var phone int
+	fmt.Println("Name: ")
+	fmt.Scan(&name)
+	fmt.Println("Age: ")
+	fmt.Scan(&age)
+	fmt.Println("HP: ")
+	fmt.Scan(&phone)
+
+	fmt.Println("My name is", name)
+	fmt.Println("I am", age, "years old")
+	fmt.Println("My phone number is", phone)
+
 }
